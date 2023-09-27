c2b0054fe98627d3c7173a93ba47b5a64aa6a541
Juan Manuel <>
2023-09-27 19:01:58 +0200

    Update Pom / Run Project

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -3,15 +3,11 @@ package com.example.demo.controllers;
 import com.example.demo.models.Users;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 public class UserController {
     @Autowired
@@ -25,4 +21,9 @@ public class UserController {
     public Users createUser(@RequestBody Users user){
         return userService.createUser(user);
     }
+    @DeleteMapping
+    public String deleteUserById(@RequestParam Integer id_user){
+        userService.deleteUser(id_user);
+        return "Ha eliminado el usuario con id: " + id_user;
+    };
 }
